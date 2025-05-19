3b7959e92e964f08fb606adc9b9ad5d7a2c695b9
Ayoub.ANEJDAM <Ayoub.ANEJDAM@AK48616>
2025-05-19 12:05:58 +0200

    feat: add index.ts in ads folder for exports

---
+++ src/ads/index.ts
@@ -0,0 +1,5 @@
+export { AdTable } from './AdTable';
+export { CreateAdModal } from './CreateAdModal';
+export { DeleteAdModal } from './DeleteAdModal';
+export { EditAdModal } from './EditAdModal';
+export { SelectProductsModal } from './SelectProductsModal';
